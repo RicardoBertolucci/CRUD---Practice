b7161fd16382b6a251edfbf513172bcce231fd69
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-03-20 11:19:52 -0300

    update: make requisition for json

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,6 +1,41 @@
+//Styles
 import './App.css';
 
+//Dependencies
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const url = "http://localhost:3000/pessoas";
+
 function App() {
+  
+  const [ pessoas, setPessoas ] = useState();
+
+  const getPersons = async () => {
+    try {
+      //Requisição com axios
+      const res = await axios.get(url);
+
+      const data = res.data;
+
+      /*
+      //Requisição com fetch
+      const res = await fetch(url);
+
+      const data = await res.json();
+      */
+
+      setPessoas(data);
+
+    } catch (e) {
+        console.error("Request for getPersons FAILED: ", e);
+    }
+  }
+  
+  useEffect(() => {
+    getPersons();
+  }, []);
+
   return (
     <div className="App">
       <h1 className="title">Requisição dos dados</h1>
